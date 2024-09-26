8e244a64c68c155c0085f166311a3c214286cd9b
n.khazh_25 <namkhanh250502@gmail.com>
2024-09-26 23:39:57 +0700

    no message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yourDate = new Date("2016-04-28T00:00:00"),
+const yourDate = new Date("2015-04-28T00:00:00"),
   music = ["myheart", "betifulinwhite", "mylove"];
 
 document.addEventListener(
