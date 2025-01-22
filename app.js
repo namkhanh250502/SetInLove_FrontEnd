fc178744f43af100db2315d5919544777abc8d2b
Nguyễn Nam Khánh <namkhanh250502@gmail.com>
2025-01-22 22:45:26 +0700

    no ms

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yourDate = new Date("2022-12-24T00:00:00"),
+const yourDate = new Date("2002-05-25T00:00:00"),
   music = ["myheart", "betifulinwhite", "mylove"];
 
 document.addEventListener(
